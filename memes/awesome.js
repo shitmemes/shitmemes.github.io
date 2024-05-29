57e05735de73aa85186f91bf38dbda00b900e78c
shitmemes <purplsoosh+shitfest@gmail.com>
2024-05-29 15:48:09 +0800

    obviously

--- memes/awesome.js
+++ memes/awesome.js
@@ -1,4 +1,14 @@
-        var id;
+fetch('https://shitmemes.github.io/memes.json')
+  .then(response => {
+    // Handle the response
+    if (!response.ok) {
+      throw new Error('network wasn\'t ok');
+    }
+    return response.json(); // Parse the response body as JSON
+  })
+  .then(memes => {
+    var id2;
+            var id;
         var title;
         var directory;
 	var note;
@@ -7,25 +17,15 @@
         var epileptic;
         var offensive;
         var mainMeme;
-
-window.onload = function() {
-    function chocolate() {
-        fetch('https://shitmemes.github.io/memes.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network skill issue');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (window.location.href.slice(-3, -1) == 'tm') {
-                    var id2 = parseInt(window.location.href.slice(0, -11).match(/-?\d+$/)?.[0], 10);
+                   if (window.location.href.slice(-3, -1) == 'tm') {
+                     id2 = parseInt(window.location.href.slice(0, -11).match(/-?\d+$/)?.[0], 10);
                 } else {
-                    var id2 = parseInt(window.location.href.slice(0, -1).match(/-?\d+$/)?.[0], 10);
+                     id2 = parseInt(window.location.href.slice(0, -1).match(/-?\d+$/)?.[0], 10);
                 }
-		for (let i = 0; i < memes.length; i++) {
+  
+  		for (let i = 0; i < memes.length; i++) {
  		   if (id2 === memes[i].id) {
-    		       id = memes[i].id;
+    		       id2 = memes[i].id;
     		       title = memes[i].title;
     		       directory = memes[i].directory;
     		       note = memes[i].note;
@@ -34,26 +34,17 @@ window.onload = function() {
  		       epileptic = memes[i].epileptic;
  		       offensive = memes[i].offensive;
  		       mainMeme = memes[i].mainMeme;
- 		       wonderhoy(id2, id, title, directory, note, src, submemes, epileptic, offensive, mainMeme);
 		    }
 		}
-            })
-            .catch(error => {
-                console.error('fetch skill issue:', error);
-            });
-    }
-    chocolate();
-};
-
-function wonderhoy(id2, id, title, directory, note, src, submemes, epileptic, offensive, mainMeme) {
-var htmlContent = "<html>" +
+  
+  var htmlContent = "<html>" +
     "<head>" +
     "<title>ID " + id + ": " + title + "</title>" +
     "<link rel='stylesheet' href='https://shitmemes.github.io/main.css'>" +
     "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
     if (id <= 0) {
-    htmlContent += "<meta property='og:video' content='" + vid + "'>" +
-    "<meta property=''content='video.movie'>" };
+    htmlContent += "<meta property='og:video' content='" + directory + "'>" +
+    "<meta property=''content='video.movie'>"; }
     htmlContent += "</head>" +
     "<body>" +
     "<div class='container'>" +
@@ -66,13 +57,13 @@ if (id % 1 != 0) {
 	htmlContent += "<a href='https://shitmemes.github.io/memes/" + (Math.floor(id) + 1) + "'>Main meme</a>";
 	} else {
 	htmlContent += "<a href='https://shitmemes.github.io/memes/" + Math.floor(id) + "'>Main meme</a>";
-	};
+	}
 	htmlContent += "--- <a href ='https://shitmemes.github.io'>Reroll</a>";
 } else if (id < 0) {
     htmlContent += "<a href='https://shitmemes.github.io/memes/" + (id + 1) + "'>Previous</a> --- <a href='https://shitmemes.github.io'>Reroll</a> --- <a href='https://shitmemes.github.io/memes/" + (id - 1) + "'>Next</a>";
 } else {
     htmlContent += "<a href='https://shitmemes.github.io/memes/" + (id - 1) + "'>Previous</a> --- <a href='https://shitmemes.github.io'>Reroll</a> --- <a href='https://shitmemes.github.io/memes/" + (id + 1) + "'>Next</a>";
-};
+}
 
     // epileptic / offensive warning
     if (epileptic === true || offensive === true) {
@@ -90,14 +81,14 @@ htmlContent += "</center>" +
     "<div class='sections'>" +
     "<div class='sectionLeft'>" +
     "<center>" +
-    "<div class='ohio'>"
+    "<div class='ohio'>";
     if (id <= 0) {
     htmlContent += "<video controls=''>" + // width='640' height='480'
-    "<source src='" + vid + "' type='video/mp4'>" +
+    "<source src='" + directory + "' type='video/mp4'>" +
     "GET A BETTER BROWSER OH MY GOD" +
-    "</video>"
+    "</video>";
     } else {
-        htmlContent += '<img width="640" height="640" src="' + vid + '" alt=' + title + '>'
+        htmlContent += '<img width="640" height="640" src="' + directory + '" alt=' + title + '>';
     }
     htmlContent += "</div></center>" +
     "</div>" +
@@ -108,13 +99,13 @@ htmlContent += "</center>" +
     "<a href='https://shitmemes.github.io'>Main page</a>" + "<br>";
     if (mainMeme) {
 		htmlContent += "<a href='https://shitmemes.github.io/memes/" + mainMeme + "'>Main meme</a><br>";
-		};
+		}
 	  if (submemes) {
 		htmlContent += "<h3>Submemes</h3><br>";
 			for (let i4 = 0; i4 < submemes.length; i4++) {
-				htmlContent += "<a href='https://shitmemes.github.io/memes/" + submemes[i4].id + "'>Submeme " + (i4 + 1) + ": " + submemes[i4].title + "</a><br>"
-			};
-		};
+				htmlContent += "<a href='https://shitmemes.github.io/memes/" + submemes[i4].id + "'>Submeme " + (i4 + 1) + ": " + submemes[i4].title + "</a><br>";
+			}
+		}
     htmlContent += "</div>" +
     "</div>" +
     "</div>" +
@@ -122,7 +113,12 @@ htmlContent += "</center>" +
     "</body>" +
     "</html>";
 	document.write(htmlContent);
-};
+  
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch
+    console.error('error.... fetched:', error);
+  });
 // ARCHIVED
 
 function balls(vid, title, id, info, offensive, epileptic, submemes) {
