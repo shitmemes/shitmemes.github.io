0e63702dc70e0b0d80c87de1f0850e781889127e
shitmemes <purplsoosh+shitfest@gmail.com>
2024-05-29 15:32:49 +0800

    WRONG JSON HOW HAVE I NOT NOTICED THIS ENTIRE TIME

--- memes/awesome.js
+++ memes/awesome.js
@@ -10,7 +10,7 @@
 
 window.onload = function() {
     function chocolate() {
-        fetch('https://shitmemes.github.io/data.json')
+        fetch('https://shitmemes.github.io/memes.json')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network skill issue');
